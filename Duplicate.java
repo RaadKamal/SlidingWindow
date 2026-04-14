442bc309dd5ae75dfce19672b6a8a2135be8f8ae
RaadKamal <43869292+RaadKamal@users.noreply.github.com>
2026-04-13 21:36:09 -0400

    Update Duplicate.java

--- Duplicate.java
+++ Duplicate.java
@@ -1,7 +1,9 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-public class SlidingWindowDuplicate {
+public class SlidingWindow {
 
     private boolean Dup(int[] num, int k) {
         Set<Integer> duplicate = new HashSet<>();
@@ -20,4 +22,60 @@ public class SlidingWindowDuplicate {
 
         return false;
     }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // The output array size is always (length - k + 1)
+        int[] output = new int[nums.length - k + 1];
+        int outIdx = 0; // Pointer for the output array
+        
+        // Deque stores indices, not values
+        Deque<Integer> q = new ArrayDeque<>();
+        int l = 0;
+        int r = 0;
+
+        while (r < nums.length) {
+            // pop smaller values from q
+            // q.peekLast() is the equivalent of q[-1]
+            while (!q.isEmpty() && nums[q.peekLast()] < nums[r]) {
+                q.pollLast(); // equivalent to q.pop()
+            }
+            q.offerLast(r); // equivalent to q.append(r)
+
+            // remove left val from window
+            // q.peekFirst() is the equivalent of q[0]
+            if (l > q.peekFirst()) {
+                q.pollFirst(); // equivalent to q.popleft()
+            }
+
+            if ((r + 1) >= k) {
+                output[outIdx] = nums[q.peekFirst()];
+                outIdx++;
+                l++;
+            }
+            r++;
+        }
+
+        return output;
+    }
+
+    public int maxProfit(int[] prices) {
+        int l = 0;
+        int r = 1;
+        int maxP = 0;
+
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                int profit = prices[r] - prices[l];
+                maxP = Math.max(maxP, profit);
+            } else {
+                l = r;
+            }
+            r++; // equivalent to r += 1
+        }
+
+        return maxP;
+    }
 }
+
+    
+
