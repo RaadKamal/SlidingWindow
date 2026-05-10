c842981818ee42c339eea53f17c1b3e11993ece3
RaadKamal <mostafaraadkamal9@gmail.com>
2026-05-09 23:44:18 -0400

    New Problem: Trapping Rain Water

---
+++ TrappingRainWater.java
@@ -0,0 +1,22 @@
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+      if (height == null || height.length == 0) {
+        return 0;
+      }
+
+      int l = 0, r = height.length - 1;
+      int leftMax = 0, rightMax = 0;
+      int total = 0;
+
+      while (l < r) {
+       =
+      }
+
+      return total;
+    }
+
+    public static void main(String[] args) {
+     
+    }
+}
