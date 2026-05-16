a1d330cb010fd99ec50b4173fbbdabd3dabeab14
RaadKamal <mostafaraadkamal9@gmail.com>
2026-05-16 01:09:39 -0400

    Starting off

--- SlidingPuzzle.java
+++ SlidingPuzzle.java
@@ -1,3 +1,18 @@
+
+
 public class SlidingPuzzle {
-    
+
+    private static final int[][] ADJACENT = {
+        {1, 3},    // neighbors for index 0
+        {0, 2, 4}, // neighbors for index 1
+        {1, 5},    // neighbors for index 2
+        {0, 4},    // neighbors for index 3
+        {1, 3, 5}, // neighbors for index 4
+        {2, 4}     // neighbors for index 5
+    };
+
+
+    public static void main(String[] args) {
+
+    }
 }
