055e907777e7cbff29657a1cedc144532238a066
RaadKamal <mostafaraadkamal9@gmail.com>
2026-05-17 17:58:18 -0400

    Updating SlidingPuzzle.java

--- SlidingPuzzle.java
+++ SlidingPuzzle.java
@@ -1,7 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.Deque;
+import java.util.HashSet;
 
 public class SlidingPuzzle {
+
+
+    private static class State {
+        int i;
+        String b;
+        int length;
+
+        State(int i, String b, int length) {
+            this.i = i;
+            this.b = b;
+            this.length = length;
+        }
+    }
+
+    Set<State> set = new HashSet<>();
+    List<State> list = new ArrayList<>();
 
     private static final int[][] ADJACENT = {
         {1, 3},    // neighbors for index 0
@@ -14,14 +33,17 @@ public class SlidingPuzzle {
 
     String seen = new String();
     // Constructor to initialize seen from a given board
+   
     public SlidingPuzzle(int[][] board) {
         for (int[] row : board) {
             for (int c : row) {
-                
                 seen += String.valueOf(c);
             }
         }
+        set.add(new State(seen.indexOf('0'), seen, 0));
     }
+
+    
 
     
     public static void main(String[] args) {
